16842a0916f0d37e4f38cc019e818f494ba8b31e
sophiakaye3166 <skaye8@gatech.edu>
2025-09-25 23:47:15 -0400

    Update info.py

--- WebDevLab01/info.py
+++ WebDevLab01/info.py
@@ -32,7 +32,7 @@ course_data = {
     }
 experience_data = {
     "Business Analyst": (["- Compiled cost data for a multi-million USDA grant application that was accepted and successfully funded ",
-                                                                          "- Organized “grand opening” operations and helped create marketing materials for several meat product lines", "- Created and updated discounted cash flows (DCF) and other relevant financial models using Microsoft Excel"],"Images/Meat.jpg"),
+                                                                          "- Organized 'grand opening' operations and helped create marketing materials for several meat product lines", "- Created and updated discounted cash flows (DCF) and other relevant financial models using Microsoft Excel"],"Images/Meat.jpg"),
     "Swim Instructor":(["- Learn-to-swim instruction for toddlers-adults, teaching technique, stamina improvement, confidence, & safety ",
                                                            "- Personally developed individualized and goal-oriented lesson plans, utilizing modern progress-tracking tools"],"Images/Swimming.jpg"),
     "Founder of Bipoc Solidarity":(["- Organization focused on resource allocation & strategic partnerships to support the BIPOC debate community"],"WebDevLab01/Images/BIPOC.jpeg")
